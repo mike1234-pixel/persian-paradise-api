390ecd7c0714ae725c62d0442994fc0e22061b07
Mike <miketandy@protonmail.com>
2023-08-05 19:13:25 +0100

    initial commit

---
+++ index.cjs
@@ -0,0 +1,71 @@
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+const helmet = require("helmet")
+const dotenv = require("dotenv").config()
+
+const app = express()
+app.use(helmet())
+app.use(cors())
+app.options("*", cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const DB = (process.env.MONGO_URI ?? "")
+  .replace("<user>", process.env.MONGO_USER ?? "")
+  .replace("<password>", process.env.MONGO_PASSWORD ?? "")
+  .replace("<dbname>", process.env.DBNAME ?? "")
+
+const moduleSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+  emoji: {
+    type: String,
+    required: true,
+  },
+  phrases: [
+    {
+      en: {
+        type: String,
+        required: true,
+      },
+      fa: {
+        informal: String,
+        formal: String,
+      },
+      emoji: String,
+    },
+  ],
+})
+
+const ModuleModel = mongoose.model("Module", moduleSchema)
+
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log("DB connection successful.")
+  })
+  .catch((err) => console.log(err))
+
+app.get("/api/modules", (req, res) => {
+  ModuleModel.find({}, (err, modules) => {
+    if (err) {
+      console.error("Error fetching data:", err)
+      res.status(500).json({ error: "Failed to fetch modules" })
+    } else {
+      console.log("Data from 'modules' collection:", modules)
+      res.json(modules)
+    }
+  })
+})
+
+app.listen(4000, () => {
+  console.log("Server listening on port 4000")
+})
