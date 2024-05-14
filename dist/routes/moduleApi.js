a2acbe0888422c16ee589b23a733c895e8eaaeff
Mike <miketandy@protonmail.com>
2024-05-14 12:52:27 +0100

    add put endpoint for phrase

--- dist/routes/moduleApi.js
+++ dist/routes/moduleApi.js
@@ -37,5 +37,30 @@ const moduleApi = (app) => {
             res.status(500).json({ error: "Failed to add phrase to module" });
         }
     }));
+    app.put("/api/modules/updatePhrase", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        const { moduleName, newPhrase } = req.body;
+        try {
+            const module = yield ModuleModel.findOne({ title: moduleName });
+            if (!module) {
+                res.status(404).json({ error: "Module not found" });
+                return;
+            }
+            const existingPhraseIndex = module.phrases.findIndex((phrase) => phrase.en === newPhrase.en);
+            if (existingPhraseIndex === -1) {
+                res.status(404).json({ error: "Phrase not found in the module" });
+                return;
+            }
+            module.phrases[existingPhraseIndex].en = newPhrase.en;
+            module.phrases[existingPhraseIndex].fa = newPhrase.fa;
+            module.phrases[existingPhraseIndex].emoji = newPhrase.emoji;
+            module.phrases[existingPhraseIndex].hint = newPhrase.hint;
+            yield module.save();
+            res.status(200).json({ message: "Phrase updated successfully", module });
+        }
+        catch (err) {
+            console.error("Error updating phrase:", err);
+            res.status(500).json({ error: "Failed to update phrase" });
+        }
+    }));
 };
 export default moduleApi;
